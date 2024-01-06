10ef0791af45544fc95669e6150c28f1e38cc946
hackedlings <pks9862728888@gmail.com>
2024-01-06 13:12:21 +0530

    Initial commit

---
+++ src/main/java/com/demo/springbootcaching/SpringBootCachingApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootcaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCachingApplication.class, args);
+	}
+
+}
